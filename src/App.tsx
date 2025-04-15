c14d39063337724588882e9f219bf08ddde25cb3
Kan <kanlowongsa20@gmail.com>
2025-04-15 18:37:49 +0700

    integrated movie feature

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,16 @@
-import "./App.css";
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { MovieListContainer } from './features/movies/containers/MovieListContainer'
+import { MovieDetailContainer } from './features/movies/containers/MovieDetailContainer'
 
 function App() {
-  return <></>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<MovieListContainer page={1} />} />
+        <Route path='/movie/:id' element={<MovieDetailContainer />} />
+      </Routes>
+    </BrowserRouter>
+  )
 }
 
-export default App;
+export default App
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,18 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ThemeProvider } from "./shared/styles/ThemeProvider";
+import "react-loading-skeleton/dist/skeleton.css";
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+const queryClient = new QueryClient();
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <ThemeProvider>
+        <QueryClientProvider client={queryClient}>
+          <App />
+        </QueryClientProvider>
+    </ThemeProvider>
+  </React.StrictMode>
+);
